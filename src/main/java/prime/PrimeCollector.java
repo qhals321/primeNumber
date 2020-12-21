7ec2c8934cf4c4db2727c56613df4531a9addcbd
bomin <bomin_93@naver.com>
2020-12-21 12:59:35 +0900

    feat(prime): supplier 구현

---
+++ src/main/java/prime/PrimeCollector.java
@@ -0,0 +1,38 @@
+package prime;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class PrimeCollector implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> {
+    @Override
+    public Supplier<Map<Boolean, List<Integer>>> supplier() {
+        return () -> new HashMap<Boolean, List<Integer>>() {{
+           put(true, new ArrayList<>());
+           put(false, new ArrayList<>());
+        }};
+    }
+
+    @Override
+    public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
+        return null;
+    }
+
+    @Override
+    public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
+        return null;
+    }
+
+    @Override
+    public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
+        return null;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return null;
+    }
+}
